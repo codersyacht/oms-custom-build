59b4fb392477acbc75d98e783e050ba03acfad2d
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-09-19 12:22:44 +0530

    build.sh

--- oc-scripts/build.sh
+++ oc-scripts/build.sh
@@ -3,6 +3,8 @@
 cd /opt/ssfs/runtime/container-scripts/imagebuild
 ./generateImages.sh --MODE=app --WAR_FILES=smcfs --EXPORT=false
 echo "Custom build completed"
+(echo "{ \"auths\": " ; sudo cat $PUSH_DOCKERCFG_PATH/.dockercfg ; echo "}") > /tmp/.dockercfg
 buildah tag om-app:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 echo "Custom tagging completed"
 buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
+echo "Custom Image Push conmpleted"
