ff16a7723bdac83428efaeafb1451ea9b6dba3d2
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-09-19 13:59:08 +0530

    build.sh

--- oc-scripts/build.sh
+++ oc-scripts/build.sh
@@ -5,11 +5,14 @@ echo "sandbox.prop copy completed"
 cd /opt/ssfs/runtime/container-scripts/imagebuild
 ./generateImages.sh --MODE=app --WAR_FILES=smcfs --EXPORT=false
 echo "Custom build completed"
+echo "Custom build completed"
+echo "Going to sleep"
+sleep 900
+echo "Woke after 15 minutes"
 (echo "{ \"auths\": " ; sudo cat $PUSH_DOCKERCFG_PATH/.dockercfg ; echo "}") > /tmp/.dockercfg
 buildah tag om-app:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 echo "Custom tagging completed"
-echo "Going to sleep"
-sleep 1800
-echo "Woke after 30 minutes"
+
+
 buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 echo "Custom Image Push conmpleted"
