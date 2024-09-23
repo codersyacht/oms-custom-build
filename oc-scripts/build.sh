735b55156cbc44b4ca1b4781d888ff1dae8ac30d
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-09-23 11:35:16 +0530

    build.sh

--- oc-scripts/build.sh
+++ oc-scripts/build.sh
@@ -8,7 +8,7 @@ echo "Custom build completed"
 (echo "{ \"auths\": " ; sudo cat $PUSH_DOCKERCFG_PATH/.dockercfg ; echo "}") > /tmp/.dockercfg
 buildah tag om-agent:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 echo "Custom tagging completed"
-buildah login --username codersyacht --password Call2@Allah docker.io
+#buildah login --username codersyacht --password Call2@Allah docker.io
 buildah push --tls-verify=false ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 # buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 echo "Custom Image Push conmpleted"
