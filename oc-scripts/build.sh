be9ec1aaf34a386deda1bc5a16794635216fcb0e
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-09-24 13:02:44 +0530

    build.sh

--- oc-scripts/build.sh
+++ oc-scripts/build.sh
@@ -3,7 +3,7 @@
 cp sandbox.prop /opt/ssfs/runtime/sandbox.prop
 echo "sandbox.prop copy completed"
 cd /opt/ssfs/runtime/container-scripts/imagebuild
-./generateImages.sh --MODE=agent --EXPORT=false
+./generateImages.sh --MODE=app --EXPORT=false
 echo "Custom build completed"
 (echo "{ \"auths\": " ; sudo cat $PUSH_DOCKERCFG_PATH/.dockercfg ; echo "}") > /tmp/.dockercfg
 buildah tag om-app:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
