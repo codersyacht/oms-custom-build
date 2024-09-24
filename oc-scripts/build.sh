b0efcf2f2f54b177373455a971b9bc942a9bddcc
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-09-24 15:47:10 +0530

    build.sh

--- oc-scripts/build.sh
+++ oc-scripts/build.sh
@@ -3,10 +3,10 @@
 cp sandbox.prop /opt/ssfs/runtime/sandbox.prop
 echo "sandbox.prop copy completed"
 cd /opt/ssfs/runtime/container-scripts/imagebuild
-./generateImages.sh --MODE=agent --EXPORT=false
+./generateImages.sh --MODE=app --WAR_FILES=smcfs --EXPORT=false
 echo "Custom build completed"
 (echo "{ \"auths\": " ; sudo cat $PUSH_DOCKERCFG_PATH/.dockercfg ; echo "}") > /tmp/.dockercfg
-buildah tag om-agent:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
+buildah tag om-app:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 echo "Custom tagging completed"
 #buildah push --tls-verify=false ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
