42d290b7e94e400ceb6f62b2d473e4734b2f7a05
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-09-25 17:03:52 +0530

    build.sh

--- oc-scripts/build.sh
+++ oc-scripts/build.sh
@@ -8,7 +8,7 @@ cd /opt/ssfs/runtime/container-scripts/imagebuild
 #./generateImages.sh --REPO=localhost --MODE=app --WAR_FILES=smcfs --EXPORT=false
 ./generateImages.sh --REPO=localhost --MODE=agent --EXPORT=false
 echo "Custom build completed"
-(echo "{ \"auths\": " ; sudo cat $PUSH_DOCKERCFG_PATH/.dockercfg ; echo "}") > /tmp/.dockercfg
+(echo "{ \"auths\": " ; sudo cat $PUSH_DOCKERCFG_PATH/.dockerconfigjson ; echo "}") > /tmp/.dockercfg
 sleep 450
 buildah tag om-agent:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 echo "Custom tagging completed"
