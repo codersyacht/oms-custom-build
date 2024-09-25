856fa3f49edad673a9cdebb6986c60cfb07a801e
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-09-25 17:24:54 +0530

    build.sh

--- oc-scripts/build.sh
+++ oc-scripts/build.sh
@@ -12,10 +12,8 @@ cat /var/run/secrets/openshift.io/push/.dockerconfigjson
 #(echo "{ \"auths\": " ; sudo cat $PUSH_DOCKERCFG_PATH/.dockerconfigjson ; echo "}") > /tmp/.dockercfg
 sudo cat $PUSH_DOCKERCFG_PATH/.dockerconfigjson > /tmp/.dockercfg
 cat /tmp/.dockercfg
-sleep 450
 buildah tag om-agent:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 echo "Custom tagging completed"
 #buildah push --tls-verify=false ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 echo "Custom Image Push conmpleted"
-sleep 900
