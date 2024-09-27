07330e5683bb1c0013dd223a4268b1e3b5949207
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-09-27 10:20:37 +0530

    Update build.sh

--- build-script/build.sh
+++ build-script/build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-
+sudo -u omsuser /bin/bash << 'EOF'
+source ~/.bashrc
 cp sandbox.prop /opt/ssfs/runtime/properties/sandbox.prop
-echo "sandbox.prop copy completed"
-#sudo -u omsuser /bin/bash << 'EOF'
-#source ~/.bashrc
+echo "sandbox.cfg copy completed"
+cd /opt/ssfs/runtime/bin
+./install3rdParty.sh yfsextn 1_0 -j /root/sources/oms-custom-build/jars/* -targetJVM EVERY
+echo "3rdPsarty jars installation completed"
 cd /opt/ssfs/runtime/container-scripts/imagebuild
 #./generateImages.sh --REPO=localhost --MODE=app --WAR_FILES=smcfs --EXPORT=false
 ./generateImages.sh --REPO=localhost --MODE=agent --EXPORT=false
 echo "Custom build completed"
 cat /var/run/secrets/openshift.io/push/.dockerconfigjson
-#(echo "{ \"auths\": " ; sudo cat $PUSH_DOCKERCFG_PATH/.dockerconfigjson ; echo "}") > /tmp/.dockercfg
 sudo cat $PUSH_DOCKERCFG_PATH/.dockerconfigjson > /tmp/.dockercfg
 cat /tmp/.dockercfg
 buildah tag om-agent:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 echo "Custom tagging completed"
-#buildah push --tls-verify=false ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 echo "Custom Image Push conmpleted"
