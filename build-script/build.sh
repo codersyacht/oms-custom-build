d6dce54ac9e589b5e8e8de59a077608fd9281b16
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-09-27 11:27:46 +0530

    build.sh

--- build-script/build.sh
+++ build-script/build.sh
@@ -4,7 +4,7 @@ echo "sandbox.cfg copy completed"
 cd /opt/ssfs/runtime/bin
 ./install3rdParty.sh yfsextn 1_0 -j /root/sources/jars/* -targetJVM EVERY
 echo "3rdPsarty jars installation completed"
-sudo -u omsuser /bin/bash << 'EOF'
+sudo -E -u omsuser /bin/bash << 'EOF'
 source ~/.bashrc
 cd /opt/ssfs/runtime/container-scripts/imagebuild
 #./generateImages.sh --REPO=localhost --MODE=app --WAR_FILES=smcfs --EXPORT=false
@@ -18,3 +18,4 @@ echo "Custom tagging completed"
 buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 sleep 900
 echo "Custom Image Push conmpleted"
+
