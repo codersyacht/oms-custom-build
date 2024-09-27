7687ffd768fb53c7d37217b8af01a68529743877
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-09-27 10:49:29 +0530

    build.sh

--- build-script/build.sh
+++ build-script/build.sh
@@ -10,10 +10,11 @@ cd /opt/ssfs/runtime/container-scripts/imagebuild
 #./generateImages.sh --REPO=localhost --MODE=app --WAR_FILES=smcfs --EXPORT=false
 ./generateImages.sh --REPO=localhost --MODE=agent --EXPORT=false
 echo "Custom build completed"
-cat /var/run/secrets/openshift.io/push/.dockerconfigjson
+sudo cat /var/run/secrets/openshift.io/push/.dockerconfigjson
 sudo cat $PUSH_DOCKERCFG_PATH/.dockerconfigjson > /tmp/.dockercfg
-cat /tmp/.dockercfg
+sudo cat /tmp/.dockercfg
 buildah tag om-agent:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 echo "Custom tagging completed"
 buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
+sleep 900
 echo "Custom Image Push conmpleted"
