2333261781dff8f10adc7ca0c6a2b10591ce80f5
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-09-27 11:08:35 +0530

    build.sh

--- build-script/build.sh
+++ build-script/build.sh
@@ -3,9 +3,9 @@ cp ../properties/sandbox.cfg /opt/ssfs/runtime/properties/sandbox.cfg
 echo "sandbox.cfg copy completed"
 cd /opt/ssfs/runtime/bin
 ./install3rdParty.sh yfsextn 1_0 -j /root/sources/jars/* -targetJVM EVERY
+echo "3rdPsarty jars installation completed"
 sudo -u omsuser /bin/bash << 'EOF'
 source ~/.bashrc
-echo "3rdPsarty jars installation completed"
 cd /opt/ssfs/runtime/container-scripts/imagebuild
 #./generateImages.sh --REPO=localhost --MODE=app --WAR_FILES=smcfs --EXPORT=false
 ./generateImages.sh --REPO=localhost --MODE=agent --EXPORT=false
