7cfb1e26baaa3999b927ca83c0185897cfe2233a
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-09-27 22:38:59 +0530

    build.sh

--- build-script/build.sh
+++ build-script/build.sh
@@ -5,15 +5,16 @@ chmod 777 -R /opt/ssfs/customization
 chown omsuser:omsuser -R /opt/ssfs/customization
 sudo -E -u omsuser /bin/bash << 'EOF'
 source ~/.bashrc
+sudo cat $PUSH_DOCKERCFG_PATH/.dockerconfigjson > /tmp/.dockercfg
 cp /opt/ssfs/customization/properties/sandbox.cfg /opt/ssfs/runtime/properties/sandbox.cfg
 echo "sandbox.cfg copy completed"
 cd /opt/ssfs/runtime/bin
 ./install3rdParty.sh yfsextn 1_0 -j /opt/ssfs/customization/jars/* -targetJVM EVERY
 echo "3rdParty jars installation completed"
 cd /opt/ssfs/runtime/container-scripts/imagebuild
+buildah login docker.io --authfile=/tmp/.dockercfg
 ./generateImages.sh --MODE=app --REPO=localhost --WAR_FILES=smcfs --DEV_MODE=true --EXPORT=false
 echo "Custom build completed"
-sudo cat $PUSH_DOCKERCFG_PATH/.dockerconfigjson > /tmp/.dockercfg
 buildah tag om-app:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 # export OUTPUT_IMAGE=codersyacht/oms-app:v1
