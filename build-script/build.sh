57d5919f708d0991cc38e289344a9bead2a62414
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-09-27 20:55:46 +0530

    build.sh

--- build-script/build.sh
+++ build-script/build.sh
@@ -11,7 +11,7 @@ cd /opt/ssfs/runtime/bin
 ./install3rdParty.sh yfsextn 1_0 -j /opt/ssfs/customization/jars/* -targetJVM EVERY
 echo "3rdParty jars installation completed"
 cd /opt/ssfs/runtime/container-scripts/imagebuild
-./generateImages.sh --REPO=localhost --DEV_MODE=true --EXPORT=false --MODE=agemt,base
+./generateImages.sh --REPO=localhost --DEV_MODE=true --EXPORT=false --MODE=base
 echo "Custom build completed"
 sudo cat $PUSH_DOCKERCFG_PATH/.dockerconfigjson > /tmp/.dockercfg
 buildah tag om-base:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
@@ -19,8 +19,8 @@ buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${
 # export OUTPUT_IMAGE=codersyacht/oms-app:v1
 # buildah tag om-app:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 # buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
-export OUTPUT_IMAGE=codersyacht/oms-agent:v1
-buildah tag om-agent:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
-buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
+#export OUTPUT_IMAGE=codersyacht/oms-agent:v1
+#buildah tag om-agent:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
+#buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 echo "Custom Image Push conmpleted"
 sleep 900
