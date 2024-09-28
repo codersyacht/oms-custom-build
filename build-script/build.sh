7f58cffdebd5e03c3249c324b8765a9a0297cdd6
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-09-28 11:39:53 +0530

    build.sh

--- build-script/build.sh
+++ build-script/build.sh
@@ -14,13 +14,12 @@ export WAR_FILES=${WAR_FILES:-"smcfs"}
 cd /opt/ssfs/runtime/container-scripts/imagebuild
 
 sudo cat $PUSH_DOCKERCFG_PATH/.dockerconfigjson > /tmp/.dockercfg
-cp /opt/ssfs/customization/properties/sandbox.cfg /opt/ssfs/runtime/properties/sandbox.cfg
-
+echo /tmp/.dockercfg
 # Generate the images
 ./generateImages.sh --REPO=$REPO --MODE=$MODE --WAR_FILES=$WAR_FILES --EXPORT=false
-
-  echo "Pushing image $REPO/om-$i:10.0"
-  buildah push --tls-verify=false --authfile=/tmp/.dockercfg $REPO/om-$i:10.0
-done
+echo "Image generated"
+echo ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
+buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
+echo "Push completed"
 
 EOF
