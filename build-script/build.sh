af7e46c126a73d587fe6cefc014251c5b8cda04f
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-09-28 11:57:34 +0530

    build.sh

--- build-script/build.sh
+++ build-script/build.sh
@@ -7,7 +7,7 @@ sudo -E -u omsuser /bin/bash << 'EOF'
 source ~/.bashrc
 
 export REPO=${REPO:-"localhost"}
-export MODE=${MODE:-"app"}
+export MODE=${MODE:-"agent"}
 export WAR_FILES=${WAR_FILES:-"smcfs"}
 
 # Go to the directory where the script is located
@@ -19,6 +19,7 @@ echo /tmp/.dockercfg
 ./generateImages.sh --REPO=$REPO --MODE=$MODE --WAR_FILES=$WAR_FILES --EXPORT=false
 echo "Image generated"
 echo ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
+buildah tag om-agent:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 echo "Push completed"
 
