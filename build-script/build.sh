bfbca114e749985db3e546055c79507874c0db5f
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-09-28 11:12:25 +0530

    build.sh

--- build-script/build.sh
+++ build-script/build.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
 
+# add your customization script here.
+
+sudo -u omsuser /bin/bash << 'EOF'
+
+source ~/.bashrc
+
+export REPO=${REPO:-"localhost"}
+export MODE=${MODE:-"app"}
+export WAR_FILES=${WAR_FILES:-"smcfs"}
+
+# Go to the directory where the script is located
+cd /opt/ssfs/runtime/container-scripts/imagebuild
+
+sudo cat $PUSH_DOCKERCFG_PATH/.dockerconfigjson > /tmp/.dockercfg
+cp /opt/ssfs/customization/properties/sandbox.cfg /opt/ssfs/runtime/properties/sandbox.cfg
+
+# Generate the images
+./generateImages.sh --REPO=$REPO --MODE=$MODE --WAR_FILES=$WAR_FILES --EXPORT=false
+
+  echo "Pushing image $REPO/om-$i:10.0"
+  buildah push --tls-verify=false --authfile=/tmp/.dockercfg $REPO/om-$i:10.0
+done
+
+EOF
