af684946d734c0f4bcbb9ae16212f644d0fda84b
Jawahar Hussain <128015499+codersyacht@users.noreply.github.com>
2024-09-28 09:35:55 +0530

    build.sh

--- build-script/build.sh
+++ build-script/build.sh
@@ -2,9 +2,9 @@
 mkdir -p /opt/ssfs/customization
 cp -r ../* /opt/ssfs/customization 
 chmod 777 -R /opt/ssfs/customization
-#chown omsuser:omsuser -R /opt/ssfs/customization
-#sudo -E -u omsuser /bin/bash << 'EOF'
-#source ~/.bashrc
+chown omsuser:omsuser -R /opt/ssfs/customization
+sudo -E -u omsuser /bin/bash << 'EOF'
+source ~/.bashrc
 sudo cat $PUSH_DOCKERCFG_PATH/.dockerconfigjson > /tmp/.dockercfg
 cp /opt/ssfs/customization/properties/sandbox.cfg /opt/ssfs/runtime/properties/sandbox.cfg
 echo "sandbox.cfg copy completed"
@@ -13,16 +13,12 @@ cd /opt/ssfs/runtime/bin
 echo "3rdParty jars installation completed"
 cd /opt/ssfs/runtime/container-scripts/imagebuild
 buildah login --authfile=/tmp/.dockercfg docker.io
-# ./generateImages.sh --MODE=app --REPO=localhost --WAR_FILES=smcfs --DEV_MODE=true --EXPORT=false
-./generateImages.sh --MODE=base --REPO=localhost --EXPORT=false
+./generateImages.sh --MODE=app,agent --REPO=localhost --WAR_FILES=smcfs --DEV_MODE=true --EXPORT=false
 echo "Custom build completed"
 buildah tag om-app:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
-# export OUTPUT_IMAGE=codersyacht/oms-app:v1
-# buildah tag om-app:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
-# buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
-#export OUTPUT_IMAGE=codersyacht/oms-agent:v1
-#buildah tag om-agent:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
-#buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
+export OUTPUT_IMAGE=codersyacht/oms-agent:v1
+buildah tag om-agent:10.0 ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
+buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 echo "Custom Image Push conmpleted"
 sleep 900
